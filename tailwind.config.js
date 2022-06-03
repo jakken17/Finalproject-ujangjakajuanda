8f6a2a94bfb53ab48dbaf08fae25d2ba9792336e
Jakken17_Register_Github <jakjuanda11@gmail.com>
2022-06-03 17:02:19 +0700

    final project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '36px',
+    },
+    extend: {
+      colors: {
+        primary: '#F59E0B',
+        dark: '#0F172A',
+      },
+      screens: {
+        '2xl':'1320px',
+      },
+    },
+  },
+  plugins: [],
+}
